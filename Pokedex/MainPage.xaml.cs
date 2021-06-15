9a78c38cddc78b1a9917c8c037d200c7e6ff4acd
Dulatr <echo.dulatr@gmail.com>
2021-06-14 21:06:01 -0600

    Make a fixed UI sizing

--- Pokedex/MainPage.xaml.cs
+++ Pokedex/MainPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using Windows.System;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
+using Windows.UI.ViewManagement;
 
 using Pokedex.ViewModels;
+
 
 namespace Pokedex
 {
@@ -14,7 +16,14 @@ namespace Pokedex
             this.InitializeComponent();
             this.DataContext = new MainPageVM();
 
+            this.SizeChanged += MainPage_SizeChanged;
+
             SearchBox.QuerySubmitted += SearchBox_QuerySubmitted;
+        }
+
+        private void MainPage_SizeChanged(object sender, Windows.UI.Xaml.SizeChangedEventArgs e)
+        {
+            ApplicationView.GetForCurrentView().TryResizeView(new Windows.Foundation.Size(1080, 720));
         }
 
         private void SearchBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
