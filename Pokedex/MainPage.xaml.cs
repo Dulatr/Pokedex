ff531c545c2423b4894cef60bad81fab309b5f35
Dulatr <echo.dulatr@gmail.com>
2021-07-04 21:36:57 -0600

    Add Xbox Controller Support
    * Checks the corewindow to expose gamepad keys

--- Pokedex/MainPage.xaml.cs
+++ Pokedex/MainPage.xaml.cs
@@ -1,10 +1,9 @@
 ﻿using Windows.System;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Input;
 using Windows.UI.ViewManagement;
 
 using Pokedex.ViewModels;
-
 
 namespace Pokedex
 {
@@ -19,6 +18,25 @@ namespace Pokedex
             this.SizeChanged += MainPage_SizeChanged;
 
             SearchBox.QuerySubmitted += SearchBox_QuerySubmitted;
+
+            Window.Current.CoreWindow.KeyDown += CoreWindow_KeyDown;
+        }
+
+        private void CoreWindow_KeyDown(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs args)
+        {
+            if (args.VirtualKey == VirtualKey.GamepadDPadUp || args.VirtualKey == VirtualKey.Up || args.VirtualKey == VirtualKey.GamepadLeftThumbstickUp)
+                DPadUp.Command.Execute("Up");
+            else if (args.VirtualKey == VirtualKey.GamepadDPadLeft || args.VirtualKey == VirtualKey.Left || args.VirtualKey == VirtualKey.GamepadLeftThumbstickLeft)
+                DPadLeft.Command.Execute("Left");
+            else if (args.VirtualKey == VirtualKey.GamepadDPadRight || args.VirtualKey == VirtualKey.Right || args.VirtualKey == VirtualKey.GamepadLeftThumbstickRight)
+                DPadRight.Command.Execute("Right");
+            else if (args.VirtualKey == VirtualKey.GamepadDPadDown || args.VirtualKey == VirtualKey.Down || args.VirtualKey == VirtualKey.GamepadLeftThumbstickDown)
+                DPadDown.Command.Execute("Down");
+            else if (args.VirtualKey == VirtualKey.GamepadA || args.VirtualKey == VirtualKey.A)
+                A_Button.Command.Execute("A");
+            else if (args.VirtualKey == VirtualKey.GamepadB || args.VirtualKey == VirtualKey.B)
+                B_Button.Command.Execute("B");
+
         }
 
         private void MainPage_SizeChanged(object sender, Windows.UI.Xaml.SizeChangedEventArgs e)
